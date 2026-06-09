b1eb770fb3f416374a036c5b43a7b3010f3997fe
faizunRBAI <faizun@royalbengal.ai>
2026-06-10 02:14:18 +0600

    fix: E241 multiple spaces after ',', E302 expected 2 blank lines, (attempt 2)

--- app/config.py
+++ app/config.py
@@ -1,5 +1,6 @@
 import os
 
+
 class Settings:
     PORT: int = 8000
     HOST: str = '0.0.0.0'
@@ -8,5 +9,6 @@ class Settings:
         for field, default in self.__class__.__annotations__.items():
             val = os.getenv(field.upper())
             setattr(self, field, type(getattr(self.__class__, field))(val) if val is not None else getattr(self.__class__, field))
+
 
 settings = Settings()
